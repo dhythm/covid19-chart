78a3c09d8fe9d1033109abe16dcb7de94705e0d2
dhythm <Yuta.Okada.20@gmail.com>
2020-04-03 01:57:27 +0900

    update

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -2,8 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import "./Chart.css";
 
+interface Case {
+  date: string;
+  confirmed: number;
+  deaths: number;
+  recovered: number;
+}
+
 const Chart = () => {
-  const [cases, setCases] = useState(null);
+  const [cases, setCases] = useState<{
+    [key: string]: Case[];
+  } | null>(null);
   useEffect(() => {
     fetch("https://pomber.github.io/covid19/timeseries.json")
       .then(response => response.json())
@@ -13,36 +22,83 @@ const Chart = () => {
   if (!cases) {
     return <></>;
   }
-  const japanCases = (cases as any)?.Japan;
-  const labels = japanCases.map(c => c.date);
-  const confirmedCases = {
-    label: "confirmed",
-    backgroundColor: "rgba(75,192,192,0.4)",
-    borderColor: "rgba(75,192,192,1)",
-    pointBorderColor: "rgba(75,192,192,1)",
-    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-    data: japanCases.map(c => c.confirmed)
-  };
-  const deathCases = {
-    label: "deaths",
-    backgroundColor: "rgba(192,75,192,0.4)",
-    borderColor: "rgba(192,75,192,1)",
-    pointBorderColor: "rgba(192, 75,192,1)",
-    pointHoverBackgroundColor: "rgba(192, 75,192,1)",
-    data: japanCases.map(c => c.deaths)
-  };
-  const recoveredCases = {
-    label: "recovered",
-    backgroundColor: "rgba(192,192,75,0.4)",
-    borderColor: "rgba(192,192,75,1)",
-    pointBorderColor: "rgba(192,192,75,1)",
-    pointHoverBackgroundColor: "rgba(192,192,75,1)",
-    data: japanCases.map(c => c.recovered)
-  };
+  console.log({ ...cases });
+
+  const shownCases = [
+    getAttr(
+      cases.Japan.map(v => v.confirmed / cases.Japan.slice(-1)[0].confirmed),
+      "Japan",
+      "rgba(75,192,192,1)"
+    ),
+    getAttr(
+      cases.China.map(v => v.confirmed / cases.China.slice(-1)[0].confirmed),
+      "China",
+      "rgba(192,75,192,1)"
+    ),
+    getAttr(
+      cases.US.map(v => v.confirmed / cases.US.slice(-1)[0].confirmed),
+      "US",
+      "rgba(192,192,75,1)"
+    ),
+    getAttr(
+      cases.Italy.map(v => v.confirmed / cases.Italy.slice(-1)[0].confirmed),
+      "Italy",
+      "rgba(75,192,75,1)"
+    ),
+    getAttr(
+      cases["Korea, South"].map(
+        v => v.confirmed / cases["Korea, South"].slice(-1)[0].confirmed
+      ),
+      "South Korea",
+      "rgba(75,75,192,1)"
+    ),
+    getAttr(
+      cases["United Kingdom"].map(
+        v => v.confirmed / cases["United Kingdom"].slice(-1)[0].confirmed
+      ),
+      "US",
+      "rgba(192,75,75,1)"
+    ),
+    getAttr(
+      cases["Taiwan*"].map(
+        v => v.confirmed / cases["Taiwan*"].slice(-1)[0].confirmed
+      ),
+      "Taiwan",
+      "rgba(75,75,75,1)"
+    )
+  ];
+  const labels = cases.Japan.map(c => c.date);
+  // const norm = japanCases.slice(-1)[0].confirmed;
+  // const labels = japanCases.map(c => c.date);
+  // const confirmedCases = {
+  //   label: "confirmed",
+  //   backgroundColor: "rgba(75,192,192,0.4)",
+  //   borderColor: "rgba(75,192,192,1)",
+  //   pointBorderColor: "rgba(75,192,192,1)",
+  //   pointHoverBackgroundColor: "rgba(75,192,192,1)",
+  //   data: japanCases.map(c => c.confirmed / norm)
+  // };
+  // const deathCases = {
+  //   label: "deaths",
+  //   backgroundColor: "rgba(192,75,192,0.4)",
+  //   borderColor: "rgba(192,75,192,1)",
+  //   pointBorderColor: "rgba(192, 75,192,1)",
+  //   pointHoverBackgroundColor: "rgba(192, 75,192,1)",
+  //   data: japanCases.map(c => c.deaths / norm)
+  // };
+  // const recoveredCases = {
+  //   label: "recovered",
+  //   backgroundColor: "rgba(192,192,75,0.4)",
+  //   borderColor: "rgba(192,192,75,1)",
+  //   pointBorderColor: "rgba(192,192,75,1)",
+  //   pointHoverBackgroundColor: "rgba(192,192,75,1)",
+  //   data: japanCases.map(c => c.recovered / norm)
+  // };
 
   const data = {
     labels,
-    datasets: [confirmedCases, deathCases, recoveredCases].map((v, i) => ({
+    // datasets: [confirmedCases, deathCases, recoveredCases].map((v, i) => ({
+    datasets: shownCases.map(v => ({
       fill: false,
       lineTension: 0.1,
       borderCapStyle: "butt",
@@ -65,5 +121,14 @@ const Chart = () => {
     </div>
   );
 };
+
+const getAttr = (data: any, label: string, palette: string) => ({
+  label,
+  backgroundColor: palette,
+  borderColor: palette,
+  pointBorderColor: palette,
+  pointHoverBackgroundColor: palette,
+  data
+});
 
 export default Chart;
